6ed2e53b951bc8b21282edc43b04ff2c4d5594f1
Dave <DaveBathnes@users.noreply.github.com>
2019-03-21 09:01:38 +0000

    Change datatables options

--- js/2016-07-18-https-grading.js
+++ js/2016-07-18-https-grading.js
@@ -4,6 +4,10 @@ $(function () {
         complete: function (results) {
             var dataSet = results.data.slice(1);
             $('#tblcatalogues').DataTable({
+                pagingType: 'simple',
+                info: false,
+                lengthChange: false,
+                searching: false,
                 data: dataSet,
                 columns: [
                     { title: "Library service", data: 0 },
