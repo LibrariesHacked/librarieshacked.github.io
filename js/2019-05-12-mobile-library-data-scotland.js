99d38a761994eb0a6786b082ebcf820651a098ce
Dave <DaveBathnes@users.noreply.github.com>
2019-05-12 14:11:55 +0100

    Adding website link

--- js/2019-05-12-mobile-library-data-scotland.js
+++ js/2019-05-12-mobile-library-data-scotland.js
@@ -11,7 +11,14 @@ $(function () {
                 data: dataSet,
                 columns: [
                     { title: 'Service', data: 0 },
-                    { title: 'Pass', data: 8 }
+                    { title: 'Pass', data: 8 },
+                    {
+                        title: 'Website',
+                        data: 9,
+                        render: function (data, type, row) {
+                            return '<a title="Website link" href="' + data + '" target="_blank">Website link</a>';
+                        }
+                    }
                 ],
                 responsive: true
             });
